26463b11160239f6e3b15ff53d433594c17bf9eb
Willpower Group <ourwillpower@gmail.com>
2026-03-12 23:37:35 +0530

    Implement add_ingredient function for database
    Added a function to insert new ingredients into the database.

--- modules/ingredient_manager.py
+++ modules/ingredient_manager.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+
+def add_ingredient(sanskrit_name, botanical_name, plant_part, form, price_per_kg):
+    
+    conn = sqlite3.connect("ingredients.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO ingredients (sanskrit_name, botanical_name, plant_part, form, price_per_kg)
+    VALUES (?, ?, ?, ?, ?)
+    """, (sanskrit_name, botanical_name, plant_part, form, price_per_kg))
+
+    conn.commit()
+    conn.close()
