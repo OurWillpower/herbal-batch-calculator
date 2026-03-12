b59e8a4cf0c6accf9c6c2ad6fdacd707ba6bffc8
Willpower Group <ourwillpower@gmail.com>
2026-03-12 23:40:59 +0530

    Update ingredient_manager.py

--- modules/ingredient_manager.py
+++ modules/ingredient_manager.py
@@ -1,15 +1,11 @@
-import sqlite3
+def get_all_ingredients():
 
-
-def add_ingredient(sanskrit_name, botanical_name, plant_part, form, price_per_kg):
-    
     conn = sqlite3.connect("ingredients.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
-    INSERT INTO ingredients (sanskrit_name, botanical_name, plant_part, form, price_per_kg)
-    VALUES (?, ?, ?, ?, ?)
-    """, (sanskrit_name, botanical_name, plant_part, form, price_per_kg))
+    cursor.execute("SELECT * FROM ingredients")
+    rows = cursor.fetchall()
 
-    conn.commit()
     conn.close()
+
+    return rows
