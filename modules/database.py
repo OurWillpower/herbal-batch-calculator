6cbe2afba4e7f317d885f1fca388990e0b1184f5
Willpower Group <ourwillpower@gmail.com>
2026-03-12 23:47:01 +0530

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -2,19 +2,22 @@ import sqlite3
 
 
 def create_ingredient_table():
+
     conn = sqlite3.connect("ingredients.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS ingredients (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sanskrit_name TEXT,
-        botanical_name TEXT,
-        plant_part TEXT,
-        form TEXT,
-        price_per_kg REAL
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS ingredients (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sanskrit_name TEXT,
+            botanical_name TEXT,
+            plant_part TEXT,
+            form TEXT,
+            price_per_kg REAL
+        )
+        """
     )
-    """)
 
     conn.commit()
     conn.close()
